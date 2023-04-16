9d013d8dce62657e09a222df076f8616311b492c
viiviii <viiiix.viiviii@gmail.com>
2023-04-16 16:20:20 +0900

    :sparkles: feat: 컨텐츠 등록 화면을 구현한다

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pancake_app/content_save_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,12 +30,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _moveToContentAdd() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => const ContentSaveScreen()),
+    );
   }
 
   @override
@@ -43,24 +43,19 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Align(
+              alignment: Alignment.topRight,
+              child: ElevatedButton(
+                onPressed: _moveToContentAdd,
+                child: Text('등록'),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
